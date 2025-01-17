e96adb22a7b422329021f3cd9ee9573701dd2604
Itami-code <ibbi824@gmail.com>
2025-01-16 21:28:09 -0500

    BasicArrow

--- Assets/Scripts/Implementations/Weapons/BasicArrow.cs
+++ Assets/Scripts/Implementations/Weapons/BasicArrow.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using BaseClasses;
 using UnityEngine;
 
 namespace Implementations.Weapons
@@ -8,20 +6,39 @@ namespace Implementations.Weapons
     public class BasicArrow : Weapon
     {
         public int damage;
+        public float speed = 10f; // Speed of the arrow
+        public float maxTravelDistance = 20f; // Maximum distance the arrow can travel before deactivating
+
+        private Vector2 startPosition;
 
         protected override void StartWrapper()
         {
             base.StartWrapper();
-            AnimationDuration = 0;
-            Ignore = new List<CharacterSheet>(new []{ParentCs});
+            AnimationDuration = maxTravelDistance / speed; // Calculate duration based on speed and distance
+            Ignore = new List<CharacterSheet> { ParentCs }; // Exclude the parent character
         }
+
         protected override void Effect(CharacterSheet cs)
         {
-            cs.DealDamage(damage);
+            cs.DealDamage(damage); // Apply damage to the target
+            gameObject.SetActive(false); // Deactivate the arrow after hitting a target
         }
+
         protected override IEnumerator PlayAnimation()
         {
-            throw new System.NotImplementedException();
+            startPosition = transform.position; // Record the starting position of the arrow
+
+            // Move the arrow downward in a straight line
+            while (Vector2.Distance(startPosition, transform.position) < maxTravelDistance)
+            {
+                // Move the arrow downward based on speed
+                transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+                yield return new WaitForSeconds(0.2f); // Wait for the next frame
+            }
+
+            // Deactivate the arrow after it has traveled the maximum distance
+            gameObject.SetActive(false);
         }
     }
 }
