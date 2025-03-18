69c3cd4ae1cff14571b90416c578f8cacc980be8
Antonio Hollerman <toniohollerprof@gmail.com>
2025-03-17 21:33:16 -0400

    damage from techniques now work on Character Sheet Atk

--- Assets/Scripts/Implementations/HitBoxes/FireHitBox.cs
+++ Assets/Scripts/Implementations/HitBoxes/FireHitBox.cs
@@ -7,12 +7,13 @@ namespace Implementations.HitBoxes
 {
     public class FireHitBox : HitBox
     {
-        public float baseDamage;
-        public float burnEffectDps;
+
         public float duration;
         public bool damageSelf;
         protected override void Effect(CharacterSheet cs)
         {
+            float baseDamage = 1.5f * parent.Atk;
+            float burnEffectDps = 0.2f * parent.Atk;
             cs.DealDamage(baseDamage, parent);
             cs.LoadEffect(((sheet, deltaTime) => sheet.DealDamage(deltaTime * burnEffectDps, parent)), duration);
         }
