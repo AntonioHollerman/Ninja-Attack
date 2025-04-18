62e85016b649491f99ca952fb933db12fcb33a1e
AntonioHollerman <toniohollerProf@gmail.com>
2025-04-18 17:12:18 -0400

    progres on fire summon

--- Assets/Scripts/Implementations/Techniques/FireSummon.cs
+++ Assets/Scripts/Implementations/Techniques/FireSummon.cs
@@ -1,12 +1,32 @@
+using System.Collections.Generic;
 using BaseClasses;
+using UnityEngine;
 
 namespace Implementations.Techniques
 {
     public class FireSummon : Technique
     {
+        public float detectDistance = 20;
         protected override void Execute()
         {
             
+        }
+
+        private GameObject GetClosestTarget(List<CharacterSheet> targets)
+        {
+            GameObject target = null;
+            float distance = detectDistance;
+
+            foreach (CharacterSheet t in targets)
+            {
+                float d = Mathf.Abs((transform.position - t.transform.position).magnitude);
+                if (d < distance)
+                {
+                    distance = d;
+                    target = t.gameObject;
+                }
+            }
+            return target;
         }
     }
 }
