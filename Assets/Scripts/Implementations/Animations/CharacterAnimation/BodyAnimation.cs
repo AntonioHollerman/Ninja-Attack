59351c5fcaf93f25a69661ccb69a214ac5519a1c
AntonioHollerman <toniohollerProf@gmail.com>
2025-04-21 19:23:05 -0400

    progress on player animation

---
+++ Assets/Scripts/Implementations/Animations/CharacterAnimation/BodyAnimation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Implementations.Animations.CharacterAnimation
+{
+    public class BodyAnimation : MonoBehaviour
+    {
+        public SpriteRenderer body;
+
+        public string hurtPath;
+        public string spellCastPath;
+        public string slashPath;
+        public string walkPath;
+
+        public Sprite[] hurtFrames;
+
+        public Sprite[] spellCastLeftFrames;
+        public Sprite[] spellCastRightFrames;
+        public Sprite[] spellCastUpFrames;
+        public Sprite[] spellCastDownFrames;
+        
+        void LateUpdate()
+        {
+            transform.rotation = Quaternion.identity; // Resets rotation to world space
+        }
+    }
+}
