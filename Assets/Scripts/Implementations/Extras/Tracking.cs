dd97d18e09b024ac119db87ab9df0105ac12b971
Antonio Hollerman <toniohollerprof@gmail.com>
2025-04-22 22:12:40 -0400

    create tracking script for camera

---
+++ Assets/Scripts/Implementations/Extras/Tracking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Implementations.Extras
+{
+    public class Tracking : MonoBehaviour
+    {
+        public GameObject target;
+
+        private void LateUpdate()
+        {
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10);
+        }
+    }
+}
