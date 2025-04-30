15f98a7c0cfd6462528f43119bf6949237d5e780
Antonio Hollerman <toniohollerprof@gmail.com>
2025-04-29 20:18:44 -0400

    finish SpawnManager.cs

--- Assets/Scripts/Implementations/Managers/SpawnManager.cs
+++ Assets/Scripts/Implementations/Managers/SpawnManager.cs
@@ -1,20 +1,79 @@
+using System.Collections;
+using BaseClasses;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Implementations.Managers
 {
     public class SpawnManager : MonoBehaviour
     {
+        private readonly Color _activeC = new Color(0.1921568f, 0.792156f, 0);
+        private readonly Color _nonactiveC = new Color(1, 0, 0);
+        
         [Header("Debug Handler")]
         public bool debugModeOn;
         public SpriteRenderer sr;
 
         [Header("Spawner Parameters")] 
+        public Collider c;
         public int instances;
-        public GameObject npcPrefabs;
+        public GameObject[] npcPrefabs;
+        public float coolDown;
         
         private float MinX => transform.position.x - (transform.localScale.x / 2);
         private float MinY => transform.position.y - (transform.localScale.y / 2);
         private float MaxX => transform.position.x + (transform.localScale.x / 2);
         private float MaxY => transform.position.y + (transform.localScale.y / 2);
+
+        private IEnumerator GoOnCoolDown()
+        {
+            c.enabled = false;
+            yield return new WaitForSeconds(coolDown);
+            c.enabled = true;
+        }
+
+        private void Spawn(GameObject prefab, float x, float y)
+        {
+            Vector3 pos = new Vector3(x, y, 0);
+            Instantiate(prefab, pos, prefab.transform.rotation);
+        }
+
+        private void Awake()
+        {
+            c.enabled = true;
+            if (!debugModeOn)
+            {
+                sr.enabled = false;
+            }
+        }
+
+        private void Update()
+        {
+            if (!debugModeOn)
+            {
+                return;
+            }
+
+            sr.color = c.enabled ? _activeC : _nonactiveC;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Player p = other.GetComponent<Player>();
+            if (p == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < instances; i++)
+            {
+                float x = Random.Range(MinX, MaxX);
+                float y = Random.Range(MinY, MaxY);
+                GameObject prefab = npcPrefabs[Random.Range(0, npcPrefabs.Length)];
+                Spawn(prefab, x, y);
+            }
+
+            StartCoroutine(GoOnCoolDown());
+        }
     }
 }
