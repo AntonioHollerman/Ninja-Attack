99f075b1e3a8eb6f89a1da2c58a685ea506a325e
AntonioHollerman <toniohollerProf@gmail.com>
2025-05-22 10:04:40 -0400

    progress on ice shield

--- Assets/Scripts/Implementations/HitBoxes/IceHitBox.cs
+++ Assets/Scripts/Implementations/HitBoxes/IceHitBox.cs
@@ -6,6 +6,7 @@ namespace Implementations.HitBoxes
     public class IceHitBox : HitBox
     {
         public GameObject iceHitMark;
+        public float absorbedDmg;
         protected override void Effect(CharacterSheet cs)
         {
             
